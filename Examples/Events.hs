1d3bce7b569f9587fc2330a98745f94e6f76d60d
windwarrior <lennartbuit@gmail.com>
2013-12-16 12:32:40 +0100

    Voorbeelden toegevoegd (een paar dan), en een watch scriptje dat je filesystem in de gaten houd en latex build

---
+++ Examples/Events.hs
@@ -0,0 +1,20 @@
+import CanvasHs
+import CanvasHs.Data
+
+-- Onze state is een getal
+type State = Int
+
+-- De initiele state
+initState = 0
+
+-- registreer jezelf bij Canvas.hs met de functie handler en de initiele state 0
+main = installEventHandler handler 0
+
+-- de functie die vanuit Canvas.hs aangeroepen wordt
+handler :: State -> Event -> (State, Output)
+handler state StartEvent = (state, shape $ Container 900 600 [Event defaults{eventId="circle", mouseClick=True} $ Circle (200, 200) 50])
+
+{- 
+    deze patternmatch wordt uitgevoerd als er een mouseclick met eventid circle is.
+-}
+handler state (MouseClick (x,y) "circle") = (state, shape $ Container 900 600 [ Event defaults{eventId="circle", mouseClick=True} $ Fill (255,0,0,1.0) $ Circle (200, 200) 50 ])
